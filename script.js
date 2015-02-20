8bac9e7a55b0e7486b2c3fc5294eef7029f5e633
kddrake <kean.drake@banno.com>
2015-02-20 08:15:44 -0600

    Created home state,home.html template with state navigation bar

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 (function (){
 	var app = angular.module('uxiApp', ['ngResource','ui.router'])
+		app.config(function($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise("/")
+			$stateProvider
+				.state('home', {
+					url: "/",
+					templateUrl:"partials/home.html"
+				});
+		});
 		app.directive('userProfile', function(){
 			return {
 				restrict: 'E',
