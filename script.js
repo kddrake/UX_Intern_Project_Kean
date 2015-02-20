7008d1a8cc443fa75de33369c91f229eeba4f476
kddrake <kean.drake@banno.com>
2015-02-20 07:38:47 -0600

    Removed profileViewController from main module in script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 (function (){
 	var app = angular.module('uxiApp', ['ngResource','ui.router'])
-		app.controller('profileViewController', function($scope){
-			$scope.sizeButton = '-'
-
-			$scope.changeButton = function(){
-				if (this.sizeButton === '-')
-					this.sizeButton = '+'
-				else
-					this.sizeButton = '-'
-			}
-		})
 		app.directive('userProfile', function(){
 			return {
 				restrict: 'E',
