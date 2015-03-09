a24b536f8c63172ff063f5263f2d677df7714978
kddrake <kean.drake@banno.com>
2015-03-09 09:00:18 -0500

    Add filtlers to user search

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,12 @@
 app.controller('usersController', function($scope, promiseFactory, userService) {
+	userService.getUsers();
 	$scope.user = angular.copy(userService.user);
-
-	promiseFactory.list()
-		.then(function(users) {
-			$scope.users = users;
-			userService.users = $scope.users;
-	}, function(error) {
-		console.log(error);
-	});
-
+	$scope.filterBy = {all: false, 
+					   firstName: false, 
+					   lastName: false, 
+					   phone: false, 
+					   email: false};
+	
 	$scope.deleteUser = function(user) {
 		userService.deleteUser(user);
 	};
@@ -16,4 +14,17 @@ app.controller('usersController', function($scope, promiseFactory, userService) 
 	$scope.$on('UPDATE_USERS', function(event, newUsersList){
 		$scope.users = angular.copy(newUsersList);
 	})
+
+	$scope.clearFilter = function(filter) {
+		if (filter === 'first') {
+			$scope.filterBy = null;
+		} else if (filter === 'last') {
+			$scope.filterBy.lastName = null;
+		} else if (filter === 'phone') {
+			$scope.filterBy.phone = null;
+		} else {
+			$scope.filterBy.email = null;
+		};
+	};
+
 });
---
+++ directives/usersPanel/filterCheckboxesDirective.js
@@ -0,0 +1,6 @@
+app.directive('filterCheckboxes', function() {
+	return {
+		restrict: 'E',
+		templateUrl:"partials/bootstrap-items/usersPanel/filterCheckboxes.html"
+	};
+});
